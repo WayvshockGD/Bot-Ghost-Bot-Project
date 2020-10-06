8e4a96bd77d8792b3746879937c8c57561ca7432
Irian3x3 <68793401+Irian3x3@users.noreply.github.com>
2020-10-06 17:12:48 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -10,7 +10,7 @@ module.exports = {
         if (!ping_js || ping_js === 'true') {
  
         message.channel.send("Ping?").then(msg => {
-            msg.edit(`Latency is ${Date.now() - message.createdTimestamp}ms Bot Latency is ${Math.round(client.ping)}ms`)
+            msg.edit(`Latency is ${Date.now() - message.createdTimestamp}ms Bot Latency is ${Math.round(client.ws.ping)}ms`)
         })
 
     }
