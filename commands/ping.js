233b79d3d6e3de9bb1c42421f7815b03b468be2f
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-10-05 21:06:16 -0400

    now your able to disable / enable commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,19 @@
+const { ping_js } = require('../config/controller/commands.json')
+
 module.exports = {
     name: 'ping',
     description: '',
     execute(message, client) {
 
+        if (!ping_js || ping_js === 'false') return message.channel.send('command disabled')
+
+        if (!ping_js || ping_js === 'true') {
  
         message.channel.send("Ping?").then(msg => {
             msg.edit(`Latency is ${Date.now() - message.createdTimestamp}ms Bot Latency is ${Math.round(client.ping)}ms`)
         })
+
+    }
 
     }
 
--- commands/say.js
+++ commands/say.js
@@ -1,17 +1,22 @@
 const { prefix } = require('../config/setting.json')
+const { say_js } = require('../config/controller/commands.json')
 
 module.exports = {
     name: 'say',
     description: '',
     execute(message, args) {
+
+        if (!say_js || say_js === 'false') return message.channel.send('command disabled')
+
+        if (!say_js || say_js === 'true') {
+
+            if(!args.join(" ")){
+                return message.reply(`Sorry, I was unable to say that. This command can be used by typing ${prefix}say <message>`)
+            }
+    
+            message.channel.send(args.join(" "));
+            
+        }
          
-        if(!args.join(" ")){
-            return message.reply(`Sorry, I was unable to say that. This command can be used by typing ${prefix}say <message>`)
-        }
-
-        message.channel.send(args.join(" "));
-        
-
-        }
-
     }
+}
